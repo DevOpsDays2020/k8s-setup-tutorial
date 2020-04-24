1afe5da23bc4edc2b3cb91c8eda8cfbc7b7e4427
liangbo <908325713@qq.com>
2020-04-24 14:36:59 +0800

    add docker-compose script

---
+++ scripts/12.docker-compose.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+
+chmod +x /usr/bin/docker-compose
